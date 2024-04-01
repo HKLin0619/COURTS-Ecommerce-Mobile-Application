08e02bc60a73bdf29240d44290c5c9cf6aacd78f
Heng Kang Lin <kanglin0619@gmail.com>
2024-04-02 00:54:41 +0800

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,9 +13,9 @@ import 'package:courts_ecommerce/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyApp(initialLocale: Locale('en')));
 
-    // Verify that our counter starts at 0.
+        // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
